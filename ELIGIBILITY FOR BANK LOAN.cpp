03069da24f43ed2472792e12dc425a84dd808f4b
JOSEPH MUSE WABUGE <wabugejoseph29@gmail.com>
2025-09-22 19:14:03 +0300

    Add files via upload

---
+++ ELIGIBILITY FOR BANK LOAN.cpp
@@ -0,0 +1,32 @@
+/*
+Name:Joseph Muse Wabuge
+Regestration no:PA106/G/29042/25
+Description:program to check eligibility for bank loan
+*/
+//main function
+
+#include <stdio.h>//preprocessor directive;scanf(),printf()
+
+int main(){
+	int age;
+	float income;
+	
+	
+	printf("Enter your income:");
+	scanf("%f",&income);
+	
+	
+	printf("Enter your age:");
+	scanf("%d",&age);
+	
+	
+	if (income>=21000&age>=21){
+		printf("congratulations you qualify for a loan.\n");
+	}else
+	{
+		printf("unfortunately we are unable to offer you a loan now.\n");
+	}
+	
+	
+	return 0;
+}
